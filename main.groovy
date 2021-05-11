7e86d1ef741bd1e2ad89d391a752c85453719905
abhinav9842 <45853526+abhinav9842@users.noreply.github.com>
2021-05-11 15:19:08 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -1,6 +1,12 @@
 def manage(){
 
     println("is this thing on")
+    def job = """job('demo') {
+    steps {
+        shell('echo Hello World!')
+    }
+}"""
+    jobDsl scriptText: job
 }
 
 return this
